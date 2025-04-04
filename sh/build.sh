496b7515c668dc3c82ec92184892ad919757a91d
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-04-04 21:28:23 +0000

    Another failure.

--- sh/build.sh
+++ sh/build.sh
@@ -2,7 +2,7 @@
 ls -1 "./go" | while IFS= read -r folder; do
 	if [ -f "./go/${folder}/main.go" ]; then
 		echob "Building \"${folder}\"..."
-		go build -o "./build/${folder}${1}" "./go/${folder}/main.go"
+		go build -o "./build/${folder}" "./go/${folder}/main.go"
 	fi
 done
 exit
---
+++ sh/goInit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+go install fyne.io/fyne/v2/cmd/fyne@latest
+go install github.com/fyne-io/fyne-cross@latest
+exit
