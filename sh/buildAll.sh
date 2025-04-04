7aeb42624be439a8542c8fc18ceedd0260f17b5e
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-04-04 21:35:15 +0000

    Revert "Another failure."
    This reverts commit 496b7515c668dc3c82ec92184892ad919757a91d.

--- sh/buildAll.sh
+++ sh/buildAll.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-if [ ! -f "$(which fyne-cross)" ]; then
-	echo "Cross-compiler for Fyne is not present."
-	exit 1
-fi
 cat ./targets.conf | while IFS= read -r target; do
 	export GOOS=$(printf $target | cut -d'_' -f1)
 	export GOARCH=$(printf $target | cut -d'_' -f2)
 	echob "Currently building on \"${GOARCH}\" for \"${GOOS}\"."
-	ls -1 ./go | while IFS= read -r folder; do
-		if [ -f "./go/${folder}/main.go" ]; then
-			echob "Cross-compiling \"${folder}\"..."
-			fyne-cross "${GOOS}" -arch=${GOARCH} -output "./build/${folder}_${target}" "./go/${folder}/main.go"
-		fi
-	done
+	shx build "_${target}"
 	echob "Cross compilation finished."
 done
 exit
--- sh/goInit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-go install fyne.io/fyne/v2/cmd/fyne@latest
-go install github.com/fyne-io/fyne-cross@latest
-exit
