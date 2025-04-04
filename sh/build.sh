b4207da3955ef84c9d9e2ce4e05bb544f7ee22ce
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-04-04 21:59:50 +0000

    Cross-compilation working for Windows amd64.

--- sh/build.sh
+++ sh/build.sh
@@ -2,7 +2,17 @@
 ls -1 "./go" | while IFS= read -r folder; do
 	if [ -f "./go/${folder}/main.go" ]; then
 		echob "Building \"${folder}\"..."
-		go build -o "./build/${folder}${1}" "./go/${folder}/main.go"
+		fileName="./build/${folder}"
+		if [ "${GOOS}" != "" ]; then
+			fileName="${fileName}_${GOOS}"
+		fi
+		if [ "${GOARCH}" != "" ]; then
+			fileName="${fileName}_${GOARCH}"
+		fi
+		if [ "${GOOS}" == "windows" ]; then
+			fileName="${fileName}.exe"
+		fi
+		go build -o "$fileName" "./go/${folder}/main.go"
 	fi
 done
 exit
--- sh/buildAll.sh
+++ sh/buildAll.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
+function getToolchain {
+	case $1 in
+		"linux")
+			printf "gcc"
+			;;
+		"windows")
+			if [ -f "$(which x86_64-w64-mingw32-gcc)" ]; then
+				printf "x86_64-w64-mingw32-gcc"
+			else
+				printf "x86_64-w64-mingw64-gcc"
+			fi
+			;;
+	esac
+}
+
+rawArch=$(uname -m)
+rootArch=$rawArch
+case $rawArch in
+	"x86_64" | "amd64")
+		rootArch="amd64"
+		;;
+	"arm64" | "armv8l" | "aarch64")
+		rootArch="arm64"
+		;;
+esac
 cat ./targets.conf | while IFS= read -r target; do
 	export GOOS=$(printf $target | cut -d'_' -f1)
 	export GOARCH=$(printf $target | cut -d'_' -f2)
+	export CC="$(getToolchain $GOOS)"
 	echob "Currently building on \"${GOARCH}\" for \"${GOOS}\"."
-	shx build "_${target}"
+	if [ "${GOOS}" == "linux" ] && [ "${GOARCH}" == "${rootArch}" ]; then
+		shx build
+	else
+		CGO_ENABLED=1 shx build
+	fi
 	echob "Cross compilation finished."
 done
 exit
