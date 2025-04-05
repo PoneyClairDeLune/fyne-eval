4bee58e4087de158d56a023021c4b3c31c17bef4
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-04-05 11:25:47 +0000

    Fyne doesn't work out in the end.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Fyne Evaluation
 How much pain do I need to experience to build a GUI app with Fyne?
+
+## Pain points
+- Debian 12 on amd64 doesn't have cross-compile toolchain to compile for Windows arm64.
+- Getting cross-compliling to work with CGO is a pain in the arsehole.
--- nix/env.nix
+++ nix/env.nix
@@ -24,14 +24,15 @@ pkgs.mkShell {
 		# Development dependencies
 		go
 		gcc
+		pkg-config
 		libGL
+		libxkbcommon
 		xorg.libX11.dev
 		xorg.libXcursor
 		xorg.libXi
 		xorg.libXinerama
 		xorg.libXrandr
 		xorg.libXxf86vm
-		libxkbcommon
 	];
 	GIT_EDITOR = "${pkgs.nano}/bin/nano";
 	shellHook = ''
