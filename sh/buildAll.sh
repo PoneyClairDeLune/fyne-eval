8078fa6430f7a0bf88feaeac22dfed801240e7f7
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-04-05 11:15:36 +0000

    Almost got arm64 cross-compiling working.

--- sh/buildAll.sh
+++ sh/buildAll.sh
@@ -2,7 +2,22 @@
 function getToolchain {
 	case $1 in
 		"linux")
-			printf "gcc"
+			case $2 in
+				"amd64")
+					if [ -f "$(which x86_64-linux-gnu-gcc)" ]; then
+						printf "x86_64-linux-gnu-gcc"
+					else
+						printf "gcc"
+					fi
+					;;
+				"arm64")
+					if [ -f "$(which aarch64-linux-gnu-gcc)" ]; then
+						printf "aarch64-linux-gnu-gcc"
+					else
+						printf "gcc"
+					fi
+					;;
+			esac
 			;;
 		"windows")
 			if [ -f "$(which x86_64-w64-mingw32-gcc)" ]; then
@@ -27,7 +42,7 @@ esac
 cat ./targets.conf | while IFS= read -r target; do
 	export GOOS=$(printf $target | cut -d'_' -f1)
 	export GOARCH=$(printf $target | cut -d'_' -f2)
-	export CC="$(getToolchain $GOOS)"
+	export CC="$(getToolchain $GOOS $GOARCH)"
 	echob "Currently building on \"${GOARCH}\" for \"${GOOS}\"."
 	if [ "${GOOS}" == "linux" ] && [ "${GOARCH}" == "${rootArch}" ]; then
 		shx build
---
+++ sh/init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+apt install -y gcc-aarch64-linux-gnu gcc-mingw-w64-x86-64
+exit
