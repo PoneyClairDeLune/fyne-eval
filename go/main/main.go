6b2ed2927c577f3c10d136d1d9dd4056951b93ce
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-04-05 15:26:09 +0000

    Fyne demo now at text updating.

--- go/main/main.go
+++ go/main/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
@@ -9,6 +12,18 @@ import (
 func main() {
 	var fyneApp fyne.App = app.New()
 	var testWindow fyne.Window = fyneApp.NewWindow("Hi Luna!")
-	testWindow.SetContent(widget.NewLabel("Praise the moon!"))
-	testWindow.ShowAndRun()
+	var testLabel *widget.Label = widget.NewLabel("Luna is the best princess.")
+	testWindow.SetContent(testLabel)
+	go func() {
+		for range time.Tick(time.Second) {
+			testLabel.SetText(time.Now().Format("Deer nommed at 03:04:05"))
+		}
+	} ()
+	testWindow.Show()
+	fyneApp.Run()
+	destructor()
+}
+
+func destructor() {
+	fmt.Println("The deer is always happy.")
 }
